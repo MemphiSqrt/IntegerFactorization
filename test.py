8231c0819e0fadd171d3716207d840103117c8dd
Wentao Wang <wwt10@163.com>
2018-06-05 20:26:56 +0800

    update

--- test.py
+++ test.py
@@ -12,15 +12,15 @@ def makeprime(limit):
 #f = open('output.csv','w')
 #print('bits\tnumber\tquadratic\trho', file = f)
 #f.close()
-for j in range(30):
-    for i in range(10, 16):            
-        f = open('output.csv','a')
-        number = makeprime(10 ** i) * makeprime(10 ** i)
-        print('{}'.format(2 * i), file = f, end = '\t')
-        print('{}'.format(number), file = f, end = '\t')
-        print('{}'.format(QuadraticSieveFactorization4.test(number)), file = f, end = '\t')
-        print('{}'.format(RhoFactorization.test(number)), file = f) 
-        f.close()
+#for j in range(30):
+#    for i in range(10, 16):            
+#        f = open('output.csv','a')
+#        number = makeprime(10 ** i) * makeprime(10 ** i)
+#        print('{}'.format(2 * i), file = f, end = '\t')
+#        print('{}'.format(number), file = f, end = '\t')
+#        print('{}'.format(QuadraticSieveFactorization4.test(number)), file = f, end = '\t')
+#        print('{}'.format(RhoFactorization.test(number)), file = f) 
+#        f.close()
 
 for j in range(10):
     for i in range(16, 18):
